dcdcf3a69e353766b79b8bc3909643e9f90d0c8f
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2022-12-14 17:40:09 +0100

    Divided fortran file in subroutines.

--- PiApprox/PiMC.f95
+++ PiApprox/PiMC.f95
@@ -1,51 +1,79 @@
 program name
 	implicit none
 	
-	integer, parameter :: n = 1E8
+	integer, parameter :: n = 1E6
 
-	integer :: i, pts_in, pts_out
-	double precision :: x(1:n), y(1:n), t1, t2
-	double precision :: x2, y2
+	double precision :: t1, t2, pi 
 
 	call cpu_time(t1)
+	call Pi1(n, pi)
+	call cpu_time(t2)
+
+	write(*,*) "Generating random numbers once: "
+	write(*,*) "n = ", n
+	write(*,*) "pi = ", pi
+	write(*,*) "time = ", t2 - t1
+
+end program name
+
+
+! =========================================
+! 	Estimate Pi using Monte Carlo method
+! 	More efficient version
+! =========================================
+subroutine Pi1(n, pi)
+	implicit none
+	integer, intent(in) :: n
+	double precision, intent(out) :: pi
+
+	integer :: i, pts_in, pts_out
+	double precision :: x(n), y(n)
+
+	pts_in = 0
+	pts_out = 0
 
 	call random_number(x)
 	call random_number(y)
 
 	do i = 1, n
 		
-		if (x(i)**2 + y(i)**2 < 1) then
+		if (x(i)**2 + y(i)**2 <= 1) then
 			pts_in = pts_in + 1
 		else
 			pts_out = pts_out + 1
 		end if
 	end do
+	
+	pi = 4.0 * pts_in / n
 
-	call cpu_time(t2)
+end subroutine Pi1
 
-	write(*,*) "Generating random numbers once: "
-	write(*,*) "pi = ", 4.0 * pts_in / n
-	write(*,*) "time = ", t2 - t1
-	write(*,*) 
+
+! =========================================
+! 	Estimate Pi using Monte Carlo method
+! 	Less efficient version
+! =========================================
+subroutine pi2(n, pi)
+	integer, intent(in) :: n
+	double precision, intent(out) :: pi
+
+	integer :: i, pts_in, pts_out
+	double precision :: x, y
 
 	pts_in = 0
 	pts_out = 0
 
-	call cpu_time(t1)
 	do i = 1, n
-		call random_number(x2)
-		call random_number(y2)
+		call random_number(x)
+		call random_number(y)
 
-		if (x2**2 + y2**2 < 1) then
+		if (x**2 + y**2 <= 1) then
 			pts_in = pts_in + 1
 		else
 			pts_out = pts_out + 1
 		end if
 	end do 
-	call cpu_time(t2)
+	
+	pi = 4.0 * pts_in / n
 
-	write(*,*) "Generating random numbers in a loop: "
-	write(*,*) "pi = ", 4.0 * pts_in / n
-	write(*,*) "time = ", t2 - t1
-
-end program name
+end subroutine pi2
